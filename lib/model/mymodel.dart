84a8a8d6bc2f966d4c6086467a07a354bd1befe8
aizaz <aizaznetwork@gmail.com>
2021-08-31 03:27:00 +0500

    Slider Provider

--- lib/model/mymodel.dart
+++ lib/model/mymodel.dart
@@ -2,7 +2,10 @@ import 'package:flutter/widgets.dart';
 
 class MyModel extends ChangeNotifier {
   int _mycount = 0;
+  int _valueHolder = 20;
   int get counter => _mycount;
+  int get slide => _valueHolder;
+
   void addData() {
     _mycount++;
     notifyListeners();
@@ -10,6 +13,11 @@ class MyModel extends ChangeNotifier {
 
   void subractData() {
     _mycount--;
+    notifyListeners();
+  }
+
+  void sliderData(double newValue) {
+    _valueHolder = newValue.round();
     notifyListeners();
   }
 }
